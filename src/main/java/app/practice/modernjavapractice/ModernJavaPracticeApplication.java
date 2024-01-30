d4cdc2aa59cc712554b07590786307d07f8228f6
KIM HYUN GI <rlagusrl928@gmail.com>
2024-01-30 19:49:17 +0900

    setup project

---
+++ src/main/java/app/practice/modernjavapractice/ModernJavaPracticeApplication.java
@@ -0,0 +1,13 @@
+package app.practice.modernjavapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernJavaPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModernJavaPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/practice/modernjavapractice/ModernJavaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package app.practice.modernjavapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernJavaPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
